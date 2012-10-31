aeb97dac40bfe1f4331a14ba6c956bedda7b23f2
Matthew Ripley <mripley@cu-cs-vm.(none)>
2012-10-30 22:27:53 -0600

    saving progress. The cloud is now open for business

--- instances.py
+++ instances.py
@@ -63,7 +63,7 @@ def create_instances(img_id, key, num, conn=None, ud_path=None):
 
     print "spinning up instances"
     cloud_init_file = open(ud_path, "r")
-    res = conn.run_instances(image_id = img_id, min_count=num, 
+    res = conn.run_instances(image_id = img_id, min_count=num, max_count=num,
                              key_name=key, user_data=cloud_init_file.read())
 
     print "waiting for instances to come up"
@@ -78,8 +78,9 @@ def write_user_txt(server_ip, outPath):
     print >>fp, "#!/bin/bash"
     
     # store the ip address in "queue_ip"
-    print >>fp, "echo %s > queue_ip"
     print >>fp, "mkdir /tmp/data-scale"
+    print >>fp, "cd /tmp/data-scale"
+    print >>fp, "echo %s > queue_ip" %(server_ip)
     print >>fp, "git clone https://github.com/mripley/CloudWork.git"
 
 #compiles the init.txt and user.txt into the combined archive
@@ -89,21 +90,21 @@ def compile_user_data(output, init, user):
     subprocess.call(cmd, shell=True)
 
     # gzip it
-    subprocess.call("gzip combined-userdata.txt", shell=True)
+    subprocess.call("gzip -f "+output, shell=True)
 
 # creates our cloud and waits for it to come up. Returns the connection and 
 # reservation  
 def create_cloud():
     # track all our reservations
     res = []
-    rabbitmq-config = "cloud-configs/combined-userdata.txt.gz"
+    rabbitmq_config = "cloud-configs/combined-userdata.txt.gz"
     
     # grab a connection 
     conn = conn_from_env()
 
     print "----CREATING QUEUE SERVER----"
     # create out rabbit mq server and wait for it to come up
-    mq_res = create_instances("ami-0000000d", "mripleykey", 1, conn, ud_path=rabbitmq-config)
+    mq_res = create_instances("ami-0000000d", "mripleykey", 1, conn, ud_path=rabbitmq_config)
     # track this req
     res.append(mq_res)
     
@@ -115,7 +116,7 @@ def create_cloud():
     config_path_base = os.path.join("cloud-configs", "worker-init")
     final_config = os.path.join(config_path_base, "combined-userdata.txt")
 
-    write_user_txt(mq_req.instances[0].private_ip_address, os.path.join(config_path_base, "user.txt"))
+    write_user_txt(mq_res.instances[0].private_ip_address, os.path.join(config_path_base, "user.txt"))
     compile_user_data(final_config, os.path.join(config_path_base, "init.txt"), 
                       os.path.join(config_path_base, "user.txt"))
 
